c42cd217a969850d717c7029a7ef8f2a973c8e1c
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2023-10-26 22:29:01 +0000

    Clase online

---
+++ entrega/g1/funciones_str.py
@@ -0,0 +1,10 @@
+str1="bienvenidos a AIPython"
+print(f"Cadena original {str1}")
+str1=str1.upper()
+print(f"Cadena despues de usar la funcion upper {str1}")
+str1=str1.lower()
+print(f"Cadena despues de usar la funcion lower {str1}")
+str1=str1.title()
+print(f"Cadena despues de usar la funcion Title {str1}")
+print(f"Usando * en cadenas {str1*3}")
+print(f"la suma de los numeros es  {2+4}")
---
+++ entrega/input.py
@@ -0,0 +1,8 @@
+num1=int(input("Ingrese Numero 1 "))
+num2=int(input("Ingrese Numero 2 "))
+suma=num1+num2
+#con argumentos posicionales
+print("Argumentos","La suma de los numero es:",suma)
+#con operador concatenacion y un casting 
+print("Con operador + La suma de los numero es:"+str(suma))
+print(f"La suma de los numeros {num1} y {num2} es igual a {suma}")
