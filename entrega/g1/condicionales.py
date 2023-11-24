1f360c847ce11a2f030a4b5c7b110d5cbc6751ed
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2023-11-24 22:29:51 +0000

    Actualizacion de ejemplo de match

--- entrega/g1/condicionales.py
+++ entrega/g1/condicionales.py
@@ -19,3 +19,17 @@ while num != 0:
 print(f"Numeros positivos {positivos}")
 print(F"Numeros negativos {negativos}")
 palabra= "AIPython P1"
+
+print("Vamos a simular los errores de internet")
+error=403
+match error:
+  case 400:
+   print ("Bad request")
+  case 404:
+    print ("Not found")
+  case 418:
+    print ("I'm a teapot")
+  case 401 | 403 | 404:
+   print ("Not allowed")
+  case _:
+    print ("Something's wrong with the internet")
