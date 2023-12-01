c46ec2c0cbedf78145a584aced203e3339e2e879
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2023-12-01 16:18:25 +0000

    ayuda sobre iteraciones

---
+++ entrega/g1/iteraciones.py
@@ -0,0 +1,12 @@
+#vamos a definir una variable
+cont=10
+#vamos a definir un bucle que queremos repetir 
+#la condiciones es lo mas importante
+while cont >= 0:
+  #usamos print y cambiamos que no salte en cada impresion sino que de un espacio
+  #en blanco
+  print(cont, end=" ")
+  #decrementamos 
+  cont=cont-1
+#cuando termina el bucle damos un print para que nos quede un salto al final
+print()
