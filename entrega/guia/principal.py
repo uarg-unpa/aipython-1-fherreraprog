844d4b13e6600ba2ad2996cfc68e0f38ebfbcd28
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2024-03-01 20:15:23 +0000

    viernes 1

---
+++ entrega/guia/principal.py
@@ -0,0 +1,18 @@
+print("Taller de \"AIPython\" P1 E2")
+print('taller de "AIPython" P1 E2')
+print("Mi edad es", 40)
+print(6/3)
+print(6//3)
+print(6%3)
+print(2**2)
+primer_nombre='Franco'
+apellido_materno='Rojas'
+print(apellido_materno)
+edad=40
+lado1=32
+Lado1=34
+primer-nombre="franco"
+primer@nombre="franco"
+primer$nombre="franco"
+1num=23
+num-1=45
