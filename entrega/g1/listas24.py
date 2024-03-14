e859da47c383ce4e4911c5ce18fa8defa378ff7b
Franco Herrera <fherrera@uarg.unpa.edu.ar>
2024-03-14 22:16:08 +0000

    viernes

---
+++ entrega/g1/listas24.py
@@ -0,0 +1,35 @@
+#creacion
+#llamando a la funcion list
+nombres=list()
+#usando los corchetes
+frutas=[]
+#darle valores iniciales
+nombres=list(['Python','AIPYTHON', 'Ambiciosa'])
+frutas=["manzana",'banana','tomate','frutilla']
+#intersion, este metodo toma el valor y lo coloca al final de la lista, la longuitud de la lista aumenta en uno
+nombres.append('franco')
+#insert, nos permite agregar un elemento en cualquier posicion
+nombres.insert(0,'Miguel')
+#mostrar la lista
+print(nombres)
+#mastrar un elemento de una determina posicion
+print(nombres[0])
+#saber la longuitud
+print(len(frutas))
+#eliminacion
+del(frutas[3])
+print(frutas)
+#recorridos
+numeros=[3,-1,3,4,5,68,8]
+suma=0
+for i in range (len(numeros)):
+    suma=suma+numeros[i]
+print (suma)
+#veamos otra variante
+for i in numeros:
+    suma=suma+i
+print(suma)
+
+
+
+
---
+++ entrega/g1/mas_sobre_funciones.py
@@ -0,0 +1,28 @@
+#Alcances y mas
+#una variable vive fuera de la funcion y se puede usar en el cuerpo de la misma
+
+num=2
+def multiplicacion(x):
+  return num*x
+print(multiplicacion(7))
+
+#
+def multi(x):
+  num=3
+  return num*x
+print(multiplicacion(7))
+
+#una variable solo vive en la funcion y ese es su alcance
+#poner en otro archivo
+def suma(x):
+  num=5
+  return x+num
+print(suma(3))
+print(num)
+
+#preguntas para los alumnos
+def mensaje():
+  alt=1
+  print("Hola estudiantes de AIPython")
+print(alt)
+
